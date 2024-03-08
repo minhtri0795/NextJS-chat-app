594e66bfadd0c9e9305dc584170cb49612ff33b7
Le Minh Tri <minhtri0795@gmail.com>
2024-03-08 12:42:34 +0700

    Update Chat.jsx

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -6,7 +6,7 @@ import ChatBox from './ChatBox.jsx'
 
 export default function Chat() {
 
-  const client = Ably.Realtime.Promise({ authUrl: '/api' })
+  const client = Ably.Realtime.Promise({ authUrl: '/api/route' })
 
   return (
     <AblyProvider client={ client }>
